bc339785c1be99484a97694b50341628f5ce60fa
jjunieko <juniorgtr123@gmail.com>
2021-02-18 17:54:05 -0300

    clone spotify

---
+++ devdacticSpotify/src/app/tabs/tabs.page.ts
@@ -0,0 +1,21 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
+ 
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss']
+})
+
+export class TabsPage {
+  @ViewChild(IonTabs) tabs: IonTabs;
+
+  selected = '';
+  progress = 42;
+  
+  constructor() {}
+ 
+  setSelectedTab() {
+    this.selected = this.tabs.getSelected();
+  }
+}
